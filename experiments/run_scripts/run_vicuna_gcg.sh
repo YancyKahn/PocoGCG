51aea0cc859b9f63b559d9ea8d21fb80b72a4899
root <root@localhost.localdomain>
2024-07-09 13:51:56 +0800

    update

---
+++ experiments/run_scripts/run_vicuna_gcg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python_script_path="../../experiments_evaluate.py"
+device="cuda:6"
+model_name="vicuna"
+
+python $python_script_path \
+        --model_name $model_name  \
+        --baseline "gcg" \
+        --padding_token "" \
+        --batch_size 512 \
+        --num_steps 200 \
+        --topk 256 \
+        --device $device
